277ac9e81d3e10bf41936a6de59534305e3e3091
Arseny271 <arsendmitr@gmail.com>
2026-08-02 23:42:24 +0400

    update to 12.9.1 (6976)

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/Theme.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/Theme.java
@@ -10758,7 +10758,9 @@ public class Theme {
     }
 
     public static Paint fillingPaint(int color) {
-        PAINT_FILLING.setColor(color);
+        if (PAINT_FILLING.getColor() != color) {
+            PAINT_FILLING.setColor(color);
+        }
         return PAINT_FILLING;
     }
 
